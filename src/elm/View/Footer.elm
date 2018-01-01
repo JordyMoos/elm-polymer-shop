a5187f83a74a4a066e46ded376a95551426618ae
Jordy Moos <jordymoos@gmail.com>
2018-01-01 13:23:16 +0100

    Add footer

--- src/elm/View/Footer.elm
+++ src/elm/View/Footer.elm
@@ -10,17 +10,30 @@ import Style.Border as Border
 
 type Styles
     = None
+    | Footer
+    | DemoLabel
 
 
 styles : List (Style Styles variation)
 styles =
     [ style None []
+    , style Footer []
+    , style DemoLabel
+        [ Color.background <| Color.rgb 32 32 32
+        , Color.text Color.white
+        ]
     ]
 
 
 view : Element Styles variation msg
 view =
     footer
-        None
-        []
-        (text "Footer")
+        Footer
+        [ paddingTop 20.0, center ]
+        (column None
+            [ center, spacing 8.0 ]
+            [ newTab "https://www.polymer-project.org/2.0/toolbox/" <|
+                el None [] (text "Made by Elm and Polymer")
+            , el DemoLabel [ center, padding 6.0, width <| px 120 ] <| text "Demo Only"
+            ]
+        )
